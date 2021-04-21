0a3b77add704861f4e8214f35214684f2bee6df3
sebranly <sebranly.pinf@gmail.com>
2021-04-21 19:45:49 +0200

    refactor: create function

--- src/scores.ts
+++ src/scores.ts
@@ -1,4 +1,5 @@
 import { CardRank } from './types';
+import { compareValues } from './utils';
 
 export const getCardPoints = (rank: CardRank) => {
   const { Ace, Eight, Jack, King, Nine, Queen, Seven, Ten } = CardRank;
@@ -53,17 +54,11 @@ export const compareCardRanks = (rank1: CardRank, rank2: CardRank) => {
   const points2 = getCardPoints(rank2);
 
   if (points1 !== 0 || points2 !== 0) {
-    if (points1 > points2) return 1;
-    if (points1 === points2) return 0;
-
-    return -1;
+    return compareValues(points1, points2);
   }
 
   const value1 = getLowCardNumericValue(rank1);
   const value2 = getLowCardNumericValue(rank2);
 
-  if (value1 > value2) return 1;
-  if (value1 === value2) return 0;
-
-  return -1;
+  return compareValues(value1, value2);
 };
