8d3680387d695cdb2c1b98c02b86024720ffa1ae
sebranly <sebranly.pinf@gmail.com>
2021-04-23 20:55:53 +0200

    feat: add distribute function

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,6 @@
-import { isSameTeam } from '../index';
+import { generateDeck } from '../cards';
+import { distributeCards, isSameTeam } from '../index';
+import { Player } from '../types';
 
 test('isSameTeam', () => {
   expect(isSameTeam(0, 1)).toBe(false);
@@ -9,4 +11,25 @@ test('isSameTeam', () => {
   expect(isSameTeam(1, 3)).toBe(true);
 
   expect(isSameTeam(2, 3)).toBe(false);
+});
+
+test('distributeCards', () => {
+  const players: Player[] = [
+    { id: 0, cards: [], name: 'a' },
+    { id: 1, cards: [], name: 'b' },
+    { id: 2, cards: [], name: 'c' },
+    { id: 3, cards: [], name: 'd' }
+  ];
+
+  distributeCards(generateDeck(), players);
+
+  expect(players[0].cards).toHaveLength(8);
+  expect(players[1].cards).toHaveLength(8);
+  expect(players[2].cards).toHaveLength(8);
+  expect(players[3].cards).toHaveLength(8);
+
+  expect(players[0].cards).toMatchSnapshot('player 0');
+  expect(players[1].cards).toMatchSnapshot('player 1');
+  expect(players[2].cards).toMatchSnapshot('player 2');
+  expect(players[3].cards).toMatchSnapshot('player 3');
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const CARDS_PER_DECK = 32;
+export const NUMBER_PLAYERS = 4;
