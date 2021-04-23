eb4d4872f7f66f2090041fdaa3f312a86d7bb724
sebranly <sebranly.pinf@gmail.com>
2021-04-23 20:00:04 +0200

    feat: implement cut

--- src/__tests__/cards.test.ts
+++ src/__tests__/cards.test.ts
@@ -1,4 +1,4 @@
-import { generateDeck, generateSuit } from '../cards';
+import { cutDeck, generateDeck, generateSuit } from '../cards';
 import { CardRank, CardSuit } from '../types';
 
 const { Ace, Eight, Jack, King, Nine, Queen, Seven, Ten } = CardRank;
@@ -22,4 +22,23 @@ test('generateDeck', () => {
   const deck = generateDeck();
   expect(deck).toMatchSnapshot();
   expect(deck).toHaveLength(32);
+});
+
+test('cutDeck', () => {
+  const deck1 = cutDeck(generateDeck(), -1);
+  const deck2 = cutDeck(generateDeck(), 0);
+  const deck3 = cutDeck(generateDeck(), 16);
+  const deck4 = cutDeck(generateDeck(), 32);
+  const deck5 = cutDeck(generateDeck(), 33);
+
+  expect(deck1).toHaveLength(32);
+  expect(deck1).toStrictEqual(deck2);
+  expect(deck2).toStrictEqual(deck3);
+  expect(deck3).toStrictEqual(deck4);
+  expect(deck4).toStrictEqual(deck5);
+
+  const deck6 = cutDeck(generateDeck(), 10);
+
+  expect(deck6).toMatchSnapshot();
+  expect(deck6).toHaveLength(32);
 });
--- src/cards.ts
+++ src/cards.ts
@@ -1,4 +1,5 @@
 import { Card, CardRank, CardSuit } from './types';
+import { CARDS_PER_DECK } from './constants';
 
 export const generateSuit = (suit: CardSuit) => {
   const { Ace, Eight, Jack, King, Nine, Queen, Seven, Ten } = CardRank;
@@ -23,4 +24,14 @@ export const generateDeck = () => {
   const cards: Card[] = [Clubs, Diamonds, Hearts, Spades].map(generateSuit).reduce((a, b) => a.concat(b));
 
   return cards;
+};
+
+export const cutDeck = (cards: Card[], cardsBefore: number) => {
+  const isInvalidCardsBefore = cardsBefore <= 0 || cardsBefore >= CARDS_PER_DECK;
+  const safeCardsBefore = isInvalidCardsBefore ? CARDS_PER_DECK / 2 : cardsBefore;
+
+  const part1 = cards.slice(0, safeCardsBefore);
+  const part2 = cards.slice(safeCardsBefore);
+
+  return [...part2, ...part1];
 };
